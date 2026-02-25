8d55e40dc292e209636b73720a4152581e14f754
Juan Felipe Hernández Molina <juanfelipehdezm@gmail.com>
2026-02-25 11:01:35 -0500

    macros on dbt

---
+++ airbnb/macros/no_empty_strings.sql
@@ -0,0 +1,10 @@
+{% macro no_empty_strings(model) %}
+    {%- set conditions = [] -%}
+    {%- for col in adapter.get_columns_in_relation(model) -%}
+        {%- if col.is_string() -%}
+            {%- do conditions.append(col.name ~ " IS NOT NULL AND " ~ col.name ~ " <> ''") -%}
+        {%- endif -%}
+    {%- endfor -%}
+
+    {{ conditions | join(' AND ') if conditions else 'TRUE' }}
+{% endmacro %}
