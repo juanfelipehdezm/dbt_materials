5cf843d4d1247c621e7532078226aab922cfb290
Juan Felipe Hernández Molina <juanfelipehdezm@gmail.com>
2026-02-26 12:46:16 -0500

    dbt fusion linting fixes

--- airbnb/analyses/full_moon_no_sleep.sql
+++ airbnb/analyses/full_moon_no_sleep.sql
@@ -2,14 +2,14 @@ WITH fullmoon_reviews AS (
     SELECT * FROM {{ ref('mart_fullmoon_reviews') }}
 )
 SELECT
-    is_full_moon,
+    is_full_moon_review,
     review_sentiment,
     COUNT(*) as reviews
 FROM
     fullmoon_reviews
 GROUP BY
-    is_full_moon,
+    is_full_moon_review,
     review_sentiment
 ORDER BY
-    is_full_moon,
+    is_full_moon_review,
     review_sentiment
