8b30a8682e37f400e3e71dea60744d9586b1e15c
Juan Felipe Hernández Molina <juanfelipehdezm@gmail.com>
2026-03-02 14:37:55 -0500

    incremental load with vars

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -18,5 +18,12 @@ WHERE review_text IS NOT NULL
 -- Only include new records for incremental runs
 -- the this variable refers to the current model being built
 {% if is_incremental() %}
-    AND review_date > (SELECT MAX(review_date) FROM {{ this }})
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
